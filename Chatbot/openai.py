2f6a79107107b506996ac54fc6d95e7a493382ea
Soham Pawar <154597186+Sohamm25@users.noreply.github.com>
2025-05-04 17:34:01 +0530

    Update and rename app.py to openai.py

---
+++ Chatbot/openai.py
@@ -0,0 +1,25 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from dotenv import load_dotenv
+import os
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+import streamlit as st
+st.title('Langchain Demo With OPENAI API')
+input_text=st.text_input("Search the topic u want")
+#down below will be changed when using llama2
+llm=ChatOpenAI(model="gpt-3.5-turbo")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
