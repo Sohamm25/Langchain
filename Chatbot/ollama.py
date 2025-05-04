b7926ba1381764d5144d7861f2d21c909a417784
Soham Pawar <154597186+Sohamm25@users.noreply.github.com>
2025-05-04 17:32:24 +0530

    Add files via upload

---
+++ Chatbot/ollama.py
@@ -0,0 +1,23 @@
+from langchain_openai import ChatOpenai
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import streamlit as st
+from dotenv import load_dotenv
+import os
+#tracked by langsmith
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+abc=ChatPromptTemplate.from_messages(
+[
+    ("system", "You are a helpful assistant. Please response to the user queries"),
+    ("user", "Question:{question}")
+]
+)
+st.title("HI this chatbot(LOCAL LLM OLLAMA) is made by Soham, search the topic u want and u will get the answer!")
+input_by_user = st.text_input("Search!")
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser(),
+chain=abc | llm | output_parser
+if st.input_by_user:
+    st.write(chain.invoke({"question": input_by_user})) 
