466c481d097e42c7f19cbfd8432c01ca26f95b35
Soham Pawar <154597186+Sohamm25@users.noreply.github.com>
2025-05-04 17:35:22 +0530

    Update ollama.py

--- Chatbot/ollama.py
+++ Chatbot/ollama.py
@@ -1,23 +1,24 @@
-from langchain_openai import ChatOpenai
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
 import streamlit as st
-from dotenv import load_dotenv
 import os
-#tracked by langsmith
+from dotenv import load_dotenv
+load_dotenv()
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-
-abc=ChatPromptTemplate.from_messages(
-[
-    ("system", "You are a helpful assistant. Please response to the user queries"),
-    ("user", "Question:{question}")
-]
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
 )
 st.title("HI this chatbot(LOCAL LLM OLLAMA) is made by Soham, search the topic u want and u will get the answer!")
-input_by_user = st.text_input("Search!")
+input_text=st.text_input("Search!")
+#this is local llm
 llm=Ollama(model="llama2")
-output_parser=StrOutputParser(),
-chain=abc | llm | output_parser
-if st.input_by_user:
-    st.write(chain.invoke({"question": input_by_user})) 
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
